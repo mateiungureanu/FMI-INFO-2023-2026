8afeeee7996a07148b597c5f1dbd18ae273e8359
mateiungureanu <msungureanu@gmail.com>
2025-11-13 08:24:02 +0200

    fix

--- Anul 3/Sem1/Sisteme Distribuite (optional)/Laborator/lab 3/lab3_2/lab3_2.go
+++ Anul 3/Sem1/Sisteme Distribuite (optional)/Laborator/lab 3/lab3_2/lab3_2.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	var s string
 	fmt.Scan(&s)
-	match, _ := regexp.MatchString("p([a-z]+)ch", s)
+	match, _ := regexp.MatchString("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}", s)
 	fmt.Println(match)
 }
